9da0269d3fd0a194a0512c003ed05b1b72c04c74
qazwsxedc121 <qazwsxedc121@gmail.com>
2021-02-26 18:31:33 +0800

    lua table

---
+++ src/state/lua_table.go
@@ -0,0 +1,42 @@
+package state
+
+//import "math"
+import "number"
+
+type luaTable struct {
+	arr []luaValue
+	_map map[luaValue]luaValue
+}
+
+func newLuaTable(nArr, nRec int) *luaTable {
+	t := &luaTable{}
+	if nArr > 0 {
+		t.arr = make([]luaValue, 0, nArr)
+	}
+	if nRec > 0 {
+		t._map = make(map[luaValue]luaValue, nRec)
+	}
+	return t
+}
+
+func (self *luaTable) get (key luaValue) luaValue {
+	key = _floatToInteger(key)
+	if idx, ok := key.(int64); ok {
+		if idx >= 1 && idx <= int64(len(self.arr)) {
+			return self.arr[idx-1]
+		}
+	}
+	return self._map[key]
+}
+
+func _floatToInteger(key luaValue) luaValue {
+	if f, ok := key.(float64); ok {
+		if i, ok := number.FloatToInteger(f); ok {
+			return i
+		}
+	}
+	return key
+}
+
+
+
